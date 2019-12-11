78705040bfcf0acb7a8e649c4c4786dcbf3cf558
kajal-patel97 <57909923+kajal-patel97@users.noreply.github.com>
2019-12-11 14:56:15 +0000

    search for employee added

--- employees_oop.py
+++ employees_oop.py
@@ -15,7 +15,41 @@ class NWEmployee(MSDBConnection):
 
 #method to get one employee by id
     def get_one_employee_by_id(self):
-        query = "SELECT * FROM Employees"
+        id = int(input('Please enter the Employee ID...'))
+        query = f"SELECT * FROM Employees WHERE EmployeeID = {id} "
+        result = self._MSDBConnection__sql_query(query)
+        while True:
+            record = result.fetchone()
+            if record is None:
+                break
+            print(f'Employee ID: {record.EmployeeID} - Name: {record.TitleOfCourtesy} {record.FirstName} {record.LastName}')
+        return 'Thank You.'
+
 #method to search for one employee by name or last name
+    def search_employee(self):
+        name = input('Enter the first name or last name of the employee...')
+        query = f"SELECT * FROM Employees WHERE FirstName LIKE '%{name}%' OR LastName = '{name}' "
+        result = self._MSDBConnection__sql_query(query)
+        while True:
+            record = result.fetchone()
+            if record is None:
+                break
+            print(f'Employee ID: {record.EmployeeID}. Name: {record.TitleOfCourtesy} {record.FirstName} {record.LastName}. Title: {record.Title}')
+        return 'Completed'
+
+
+
+
 
 #add all these to the run_products
+
+table_employees = NWEmployee()
+
+# #Search for employee
+# employee = table_employees.search_employee()
+# print(employee)
+
+
+# # get one employee by id
+# employee = table_employees.get_one_employee_by_id()
+# print(employee)
